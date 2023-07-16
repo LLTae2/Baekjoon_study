c57a8bf9229ccb986871e6a256140aa9bb597eea
이태현 <80657114+LLTae2@users.noreply.github.com>
2023-07-16 15:26:27 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 708 ms, Memory: 79492 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+v, e, start = map(int, input().split())
+
+graph = [[] for _ in range(v+1)]
+visited = [False] * (v+1)
+
+for _ in range(e):
+	a, b = map(int, input().split())
+	graph[a].append(b)
+	graph[b].append(a)
+
+answer = [-1 for _ in range(v+1)]
+
+def dfs(v, cnt):
+	answer[v] = cnt
+	visited[v] = True
+	for i in sorted(graph[v]):
+		if not visited[i]:
+			dfs(i, cnt+1)
+
+dfs(start, 0)
+for i in range(1, v+1):
+	print(answer[i])
